caa7b16d56f46e12fed1ff0e5e4cbeef065966b7
Ryan Duff <ryanduff613@gmail.com>
2020-03-25 00:27:14 -0700

    place term in span with corresponding id, use jquery function to display definitionand hide term when user clicks on term

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
   
-  $('.term-showing').click(function(){
-    $('.definition-hidden').removeClass();
-    $('.definition-hidden').addClass('.definition-showing');
-    $('.term-showing').toggleClass();
-    //$('.term-showing').addClass('term-hidden');
+  $('#javascript-showing').click(function(){
+    $('.definition-hidden').show();
+    $('#javascript-showing').hide();
   });
 
   $('.definition-showing').click(function(){
-    $('term-hidden').removeClass();
-    $('term-hidden').addClass('.term-showing');
+    
   });
 
 
